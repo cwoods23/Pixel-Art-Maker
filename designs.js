2b20ab1885f469333ed9aa747d9c6ea81141f101
Colin Woods <cgwoods2311@gmail.com>
2021-02-18 11:47:59 -0500

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,28 @@
+// color input
+var color = document.getElementById("colorPicker");
+
+// size input
+var canvas = document.getElementById("pixelCanvas");
+var size = document.getElementById("sizePicker");
+var inputHeight = document.getElementById("inputHeight");
+var inputWidth = document.getElementById("inputWidth");
+
+
+size.addEventListener("submit", function(event) {
+    event.preventDefault();
+    makeGrid(inputHeight.value, inputWidth.value);
+});
+
+// makeGrid function
+function makeGrid(inputHeight, inputWidth) {
+    canvas.innerHTML = "";
+    for (var h = 0; h < inputHeight; h++) {
+        var row = canvas.insertRow(h);
+        for (var w= 0; w < inputWidth; w++) {
+            var cell = row.insertCell(w);
+            canvas.addEventListener("click", function(event) {
+                event.target.style.backgroundColor = color.value;
+            });
+        }
+    }
+};
